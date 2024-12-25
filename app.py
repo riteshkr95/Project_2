0440a9c3ae30c60055fe7d51a4ee6fb5b5be57aa
Ritesh Kumar <riteshkumar95@hotmail.com>
2024-12-25 13:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def sumbit():
         return "you are safe"
     else:
         return "you are not safe"
-app.run(debug=True ,port=8989 , host="0.0.0.0")
+app.run(debug=True ,port=5000, host="0.0.0.0")
